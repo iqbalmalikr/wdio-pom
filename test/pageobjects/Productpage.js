002997dcfbc462fe3e64c513083678902bfef158
iqbalmalikr <86758469+iqbalmalikr@users.noreply.github.com>
2024-09-01 01:31:48 +0700

    Update Productpage.js

--- test/pageobjects/Productpage.js
+++ test/pageobjects/Productpage.js
@@ -2,21 +2,21 @@ import Page from "./page.js"
 import { $ } from "@wdio/globals"
 
 class ProductPage {
-    // Define selectors using getter methods
+    
     get addToCartButton() {
-        return $('btn_primary.btn_small.btn_inventory'); // Pastikan selector ini sesuai
+        return $('btn_primary.btn_small.btn_inventory'); 
     }
 
     get cartIcon() {
-        return $('shopping-cart-link'); // Selector untuk ikon keranjang belanja
+        return $('shopping-cart-link'); 
     }
 
-    // Method to add item to cart
+    
     async addItemToCart() {
         await this.addToCartButton.click();
     }
 
-    // Method to navigate to the cart
+   
     async goToCart() {
         await this.cartIcon.click();
     }
